088298eda1aa4b8e37eecf364464c98a87cc70ac
kaikycruz <kaikycruz20@gmail.com>
2024-06-16 19:04:41 -0300

    feat: ajustando home

--- carona/src/App.js
+++ carona/src/App.js
@@ -1,51 +1,57 @@
 // Importando os elementos para poder navegar entre as páginas
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import styles from "./index.css";
 
 // Importando as páginas
-import Home from './components/pages/home/Home'
-import Chat from './components/pages/chat/Chat'
-import Login from './components/pages/login/Login'
-import Perfil from './components/pages/meu-perfil/Perfil'
-import Caronas from './components/pages/caronas/Caronas'
-import Viagens from './components/pages/viagens/Viagens'
-import Feedback from './components/pages/feedback/Feedback'
-import Cadastro from './components/pages/cadastro/Cadastro'
-import Transacoes from './components/pages/transacoes/Transacoes'
-import Fidelizados from './components/pages/fidelizados/Fidelizados'
-import CadastroCarro from './components/pages/cadastrar_carro/CadastroCarro'
-import ChatContainer from './components/pages/chat/chat_container/ChatContainer'
-import RedefinirSenha from './components/pages/redefinir_senha/RedefinirSenha'
-import OferecerCarona from './components/pages/oferecer_carona/OferecerCarona'
-import ProcurarCarona from './components/pages/procurar_carona/ProcurarCarona'
-import DetalhesViagem from './components/pages/detalhes_viagem/DetalhesViagem'
+import Home from "./components/pages/home/Home";
+import Chat from "./components/pages/chat/Chat";
+import Login from "./components/pages/login/Login";
+import Perfil from "./components/pages/meu-perfil/Perfil";
+import Caronas from "./components/pages/caronas/Caronas";
+import Viagens from "./components/pages/viagens/Viagens";
+import Feedback from "./components/pages/feedback/Feedback";
+import Cadastro from "./components/pages/cadastro/Cadastro";
+import Transacoes from "./components/pages/transacoes/Transacoes";
+import Fidelizados from "./components/pages/fidelizados/Fidelizados";
+import CadastroCarro from "./components/pages/cadastrar_carro/CadastroCarro";
+import ChatContainer from "./components/pages/chat/chat_container/ChatContainer";
+import RedefinirSenha from "./components/pages/redefinir_senha/RedefinirSenha";
+import OferecerCarona from "./components/pages/oferecer_carona/OferecerCarona";
+import ProcurarCarona from "./components/pages/procurar_carona/ProcurarCarona";
+import DetalhesViagem from "./components/pages/detalhes_viagem/DetalhesViagem";
 import PerfilUser from "./components/pages/perfil/PerfilUser";
 
 // Importando elementos de layout
-import Container from './components/layout/container/Container'
+import Container from "./components/layout/container/Container";
 
-import 'mapbox-gl/dist/mapbox-gl.css';
-
+import "mapbox-gl/dist/mapbox-gl.css";
 
 function App() {
   return (
     <Router>
       <Container customClass="min-height">
+        <ToastContainer className={styles["toast-body"]} />
         <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route path='/login' element={<Login />} />
-          <Route path='/caronas' element={<Caronas />} />
-          <Route path='/cadastro' element={<Cadastro />} />
-          <Route path='/redefinir-senha' element={<RedefinirSenha />} />
-          <Route path='/meu-perfil' element={<Perfil />} />
+          <Route exact path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/caronas" element={<Caronas />} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/redefinir-senha" element={<RedefinirSenha />} />
+          <Route path="/meu-perfil" element={<Perfil />} />
           <Route path="/perfil/:idUser" element={<PerfilUser />} />
-          <Route path='/viagens/:idUser' element={<Viagens />} />
+          <Route path="/viagens/:idUser" element={<Viagens />} />
           <Route path="/chat" element={<ChatContainer />} />
-          <Route path='/transacoes/:idUser' element={<Transacoes />} />
-          <Route path='/fidelizados/:idUser' element={<Fidelizados />} />
-          <Route path='/viagens/oferecer' element={<OferecerCarona />} />
-          <Route path='/viagens/procurar' element={<ProcurarCarona />} />
-          <Route path='/viagens/detalhes/:idViagem' element={<DetalhesViagem />} />
-          <Route path='/feedback/:idUser' element={<Feedback />} />
+          <Route path="/transacoes/:idUser" element={<Transacoes />} />
+          <Route path="/fidelizados/:idUser" element={<Fidelizados />} />
+          <Route path="/viagens/oferecer" element={<OferecerCarona />} />
+          <Route path="/viagens/procurar" element={<ProcurarCarona />} />
+          <Route
+            path="/viagens/detalhes/:idViagem"
+            element={<DetalhesViagem />}
+          />
+          <Route path="/feedback/:idUser" element={<Feedback />} />
           <Route path="/carros/:idUser" element={<CadastroCarro />} />
         </Routes>
       </Container>
--- carona/src/components/layout/slider/Slider.jsx
+++ carona/src/components/layout/slider/Slider.jsx
@@ -18,49 +18,51 @@ import img5 from "../../../utils/assets/imgSlide5.svg";
 const Slider = () => {
   return (
     <>
-      <h2 className={styles["titulo"]}>No CarOna ...</h2>
+      <div className={styles["container"]}>
+        <h2 className={styles["titulo"]}>No CarOna ...</h2>
 
-      <Swiper
-        className={styles["carrossel"]}
-        modules={[Navigation, Pagination, Scrollbar, A11y]}
-        slidesPerView={1}
-        navigation
-      >
-        <SwiperSlide className={styles["slide"]}>
-          <img src={img1} alt="Imagem 1" />
-          <h4>
-            Você oferece ou reserva carona de uma maneira simples e segura
-          </h4>
-        </SwiperSlide>
-        <SwiperSlide className={styles["slide"]}>
-          <img src={img2} alt="Imagem 2" />
-          <h4>
-            Você realiza pagamentos sem dor de cabeça e conta com Seguro CaRona
-            para sua segurança
-          </h4>
-        </SwiperSlide>
-        <SwiperSlide className={styles["slide"]}>
-          <img src={img3} alt="Imagem 3" />
-          <h4>
-            Você avalia o motorista ou passageiro e contribui com a comunidade
-            CaRona
-          </h4>
-        </SwiperSlide>
-        <SwiperSlide className={styles["slide"]}>
-          <img src={img4} alt="Imagem 4" />
-          <h4>
-            As mulheres viajam com mais segurança, selecionando a opção de
-            viajar apenas com mulheres
-          </h4>
-        </SwiperSlide>
-        <SwiperSlide className={styles["slide"]}>
-          <img src={img5} alt="Imagem 5" />
-          <h4>
-            Você pode ter sua carona reservada automaticamente, se tornando um
-            passageiro fiel
-          </h4>
-        </SwiperSlide>
-      </Swiper>
+        <Swiper
+          className={styles["carrossel"]}
+          modules={[Navigation, Pagination, Scrollbar, A11y]}
+          slidesPerView={1}
+          navigation
+        >
+          <SwiperSlide className={styles["slide"]}>
+            <img src={img1} alt="Imagem 1" />
+            <h4>
+              Você oferece ou reserva carona de uma maneira simples e segura
+            </h4>
+          </SwiperSlide>
+          <SwiperSlide className={styles["slide"]}>
+            <img src={img2} alt="Imagem 2" />
+            <h4>
+              Você realiza pagamentos sem dor de cabeça e conta com Seguro
+              CaRona para sua segurança
+            </h4>
+          </SwiperSlide>
+          <SwiperSlide className={styles["slide"]}>
+            <img src={img3} alt="Imagem 3" />
+            <h4>
+              Você avalia o motorista ou passageiro e contribui com a comunidade
+              CaRona
+            </h4>
+          </SwiperSlide>
+          <SwiperSlide className={styles["slide"]}>
+            <img src={img4} alt="Imagem 4" />
+            <h4>
+              As mulheres viajam com mais segurança, selecionando a opção de
+              viajar apenas com mulheres
+            </h4>
+          </SwiperSlide>
+          <SwiperSlide className={styles["slide"]}>
+            <img src={img5} alt="Imagem 5" />
+            <h4>
+              Você pode ter sua carona reservada automaticamente, se tornando um
+              passageiro fiel
+            </h4>
+          </SwiperSlide>
+        </Swiper>
+      </div>
     </>
   );
 };
